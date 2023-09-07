fdf24883fa36e324675106be1695b59d76179ce9
Nivedha22 <nivedhanarayanan2002@gmail.com>
2023-09-07 19:19:23 +0530

    Login page Boilercode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
-import About from './pages/About';
-import Team from './pages/Team';
-function App() {
+import React, { Component } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export default function App() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
   return (
-    <div className="App">
-      <header>
-        <>
-          <nav>
-            <ul>
-              <li><Link to="/About">About</Link></li>
-              <li><Link to="/Team">Team</Link></li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path='/About' element={<About />} />
-            <Route path='/Team' element={<Team />} />
-          </Routes>
-        </>
-      </header>
-    </div>
+   
+    <Container component="main" maxWidth="sm">
+      <div className="App-header">
+          <Typography  component="h1" variant="h5">
+          Welcome to WF NetBanking
+        </Typography>
+        </div>
+      <Box 
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button 
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  
+   
   );
 }
-
-export default App;
