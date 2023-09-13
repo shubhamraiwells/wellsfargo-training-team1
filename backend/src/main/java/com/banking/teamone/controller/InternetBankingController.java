7d09c186acd69ac3c73f4ee8185d558fd6864f85
shubhamraiwells <shubhamyouhavetocode@gmail.com>
2023-09-13 08:32:39 +0530

    debug jwt

--- backend/src/main/java/com/banking/teamone/controller/InternetBankingController.java
+++ backend/src/main/java/com/banking/teamone/controller/InternetBankingController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/test")
 public class InternetBankingController {
 Logger logger= LoggerFactory.getLogger(InternetBankingController.class);
 
