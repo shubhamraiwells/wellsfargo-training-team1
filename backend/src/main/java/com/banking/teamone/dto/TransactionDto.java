457ea5817e7c62e2e6eac574814035e1b3909451
shubhamraiwells <shubhamyouhavetocode@gmail.com>
2023-09-14 16:10:29 +0530

    added transaction dto service and controller

---
+++ backend/src/main/java/com/banking/teamone/dto/TransactionDto.java
@@ -0,0 +1,33 @@
+package com.banking.teamone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TransactionDto {
+
+    private Integer id;
+
+
+
+    private String fromAccountNo;
+
+
+
+    private String toAccountNo;
+
+
+    private BigDecimal transactionAmount;
+
+
+    private  LocalDateTime transactionDate= LocalDateTime.now();
+}
---
+++ backend/src/main/java/com/banking/teamone/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.banking.teamone.repository;
+
+import com.banking.teamone.dto.TransactionDto;
+import com.banking.teamone.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
+      Transaction findByFromAccountNo(String accountNo);
+}
