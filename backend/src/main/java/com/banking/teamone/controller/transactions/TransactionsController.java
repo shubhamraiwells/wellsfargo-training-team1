b345319babe3ef5944eb66edbd3e24f9156d30c5
shubhamraiwells <shubhamyouhavetocode@gmail.com>
2023-09-14 15:45:45 +0530

    added transaction model

---
+++ backend/src/main/java/com/banking/teamone/controller/transactions/TransactionsController.java
@@ -0,0 +1,14 @@
+package com.banking.teamone.controller.transactions;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionsController {
+
+
+
+
+}
