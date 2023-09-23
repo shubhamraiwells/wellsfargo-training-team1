611a79b0ee0b51db2405c9e1c2787515ef5397cc
shubhamraiwells <shubhamyouhavetocode@gmail.com>
2023-09-23 11:51:26 +0530

    added account lock features after multiple unsuccessful attempts

--- backend/src/main/java/com/banking/teamone/service/CustomerIbService.java
+++ backend/src/main/java/com/banking/teamone/service/CustomerIbService.java
@@ -7,12 +7,16 @@ import com.banking.teamone.repository.CustomerIbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 
 @Service
 public class CustomerIbService {
 
+    public static final int MAX_FAILED_ATTEMPTS = 3;
+
+    private static final long LOCK_TIME_DURATION =  10000; // 24 hours
     @Autowired
     CustomerConverter customerConverter;
 
@@ -29,8 +33,40 @@ public class CustomerIbService {
 
 
     public CustomerIb createCustomerIb(CustomerIb customerIb){
-        return customerIbRepository.save(customerIb);
+        return customerIbRepository.save(new CustomerIb(customerIb.getUsername(),customerIb.getPassword(),customerIb.getRole(),customerIb.getAccountNo(),
+                customerIb.getIsActive(),customerIb.isAccountNonLocked(),customerIb.getFailedAttempt(),customerIb.getLockTime()));
+    }
+    public void increaseFailedAttempts(CustomerIb user) {
+        int newFailAttempts = user.getFailedAttempt() + 1;
+        customerIbRepository.updateFailedAttempts(newFailAttempts, user.getUsername());
     }
 
+    public void resetFailedAttempts(String username) {
+        customerIbRepository.updateFailedAttempts(0, username);
+    }
+
+    public void lock(CustomerIb user) {
+        user.setAccountNonLocked(false);
+        user.setLockTime(new Date());
+
+        customerIbRepository.save(user);
+    }
+
+    public boolean unlockWhenTimeExpired(CustomerIb user) {
+        long lockTimeInMillis = user.getLockTime().getTime();
+        long currentTimeInMillis = System.currentTimeMillis();
+
+        if (lockTimeInMillis + LOCK_TIME_DURATION < currentTimeInMillis) {
+            user.setAccountNonLocked(true);
+            user.setLockTime(null);
+            user.setFailedAttempt(0);
+
+            customerIbRepository.save(user);
+
+            return true;
+        }
+
+        return false;
+    }
 
 }
